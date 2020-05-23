5024d8600c9dd65c7969c7ca8f9de9422aedcb12
karannagi <karan.nagi@gmail.com>
2020-05-24 00:20:17 +0400

    Create 1.java

---
+++ src/com/leetcode/practice/1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       Map<Integer,Integer> numberSet = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            numberSet.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(numberSet.containsKey(target-nums[i]) && i!=numberSet.get(target-nums[i]))              
+                return new int[]{i,numberSet.get(target-nums[i])};
+                                }
+    return null;        
+    }
+        }
